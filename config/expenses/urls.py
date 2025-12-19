664d4aafb34f3e33e03ec02d53f0e1caa4219226
Challu59 <aayushadhikari72@gmail.com>
2025-12-19 13:32:49 +0545

    modify invite view and add invite url

--- config/expenses/urls.py
+++ config/expenses/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CustomTokenObtainPairView, RegisterView, GroupListCreateView, GroupDetailView
+from .views import CustomTokenObtainPairView, RegisterView, GroupListCreateView, GroupDetailView, GroupInviteView
 
 urlpatterns = [
     path('auth/login/', CustomTokenObtainPairView.as_view(), name= 'login'),
     path('auth/register/', RegisterView.as_view()),
     path('groups/', GroupListCreateView.as_view()),
-    path('groups/<int:pk>', GroupDetailView.as_view()),
+    path('groups/<int:id>', GroupDetailView.as_view()),
+    path('groups/<int:id>/invite', GroupInviteView.as_view()),
 ]
--- config/expenses/views.py
+++ config/expenses/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import CustomTokenObtainPairSerializer, UserSerializer, GroupSerializer, GroupMemberSerializer, JoinGroupSerializer
 from .models import User, Group, GroupMember
@@ -45,3 +47,31 @@ class GroupDetailView(generics.RetrieveAPIView):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class GroupInviteView(APIView):
+    permission_classes = permissions.IsAuthenticated
+
+    def post(self, request, id):
+        group = get_object_or_404(Group, id=id)
+
+        if(group.created_by)!=(request.user):
+            return Response(
+                {"detail" : "Only the group creator can invite members"},
+                status = status.HTTP_403_FORBIDDEN       
+                )
+        
+        user_id = request.data.get(user_id)
+        user = get_object_or_404(User, id = id)
+
+        if(GroupMember.filter(group = group,user = user).exists()):
+            return Response(
+                {"detail" : "User is already in the group"},
+                status= status.HTTP_400_BAD_REQUEST,
+            )
+        
+        GroupMember.objects.create(group=group, user=user)
+
+        return Response(
+            {"detail": "User added to the group successfully"},
+            status= status.HTTP_201_CREATED
+        )
