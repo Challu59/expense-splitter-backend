35b1fa2fcf193c685ce72666bcd3a86d9505cd47
Challu59 <aayushadhikari72@gmail.com>
2025-12-21 23:52:27 +0545

    add expense and expense split model

--- config/expenses/models.py
+++ config/expenses/models.py
@@ -40,3 +40,29 @@ class GroupMember(models.Model):
 
     def __str__(self):
         return f"{self.user.name} in {self.group.name}"
+
+class Expense(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='expenses')
+    paid_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='paid_expenses')
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    descriptionn = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.description} - {self.amount}"
+    
+class ExpenseSplit(models.Model):
+    expense = models.ForeignKey(
+        Expense,
+        on_delete=models.CASCADE,
+        related_name="splits"
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="expense_splits"
+    )
+    share_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user.email} owes {self.share_amount}"
